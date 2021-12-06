182c34c1e260f91c7e0dd5dff99a53736a3d5b99
Nathan Bleigh <nathan.bleigh@gmail.com>
2021-12-06 07:29:34 -0600

    day 6 comments

--- lib/Submarine/Lanternfish.hs
+++ lib/Submarine/Lanternfish.hs
@@ -11,17 +11,26 @@ import Text.Megaparsec.Extras (Parser, intP)
 parseSweep :: Parser [Int]
 parseSweep = sepBy intP (char ',')
 
+-- Predict the number of lanternfish that will be around
+-- after N days
 countAfterDaysN :: Int -> [Int] -> Int
 countAfterDaysN n init = project initialMap n |> sum
  where
   initialMap = Map.fromListWith (+) [(i, 1) | i <- init]
 
+-- Project accepts an initial state, and how many days
+-- out to project the lanternfish state, and iterates
+-- the state forward day by day
 project :: Map Int Int -> Int -> Map Int Int
 project xs 0 = xs
 project xs n = project next ((-) n 1)
  where
   next = Map.fromListWith (+) [b | a <- Map.toList xs, b <- step a]
 
+-- Given a single timer + count value, iterate
+-- to the next timer + count value / values
+-- (in the case more than one are produced they should
+-- be summed (see above))
 step :: (Int, Int) -> [(Int, Int)]
 step (t, count)
   | t == 0 = [(6, count), (8, count)]
